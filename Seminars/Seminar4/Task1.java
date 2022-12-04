ace72fb5b6f83e1b04d2c25eb231a50bfd7e6177
«AKoshelya» <design-kos@ya.ru>
2022-12-04 11:54:21 +0500

    Add Task1(Seminar4)

---
+++ Seminars/Seminar4/Task1.java
@@ -0,0 +1,51 @@
+package Seminars.Seminar4;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Укажите размер массива: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        LinkedList<Integer> arrayOfNums = getRandomArray(n);
+        System.out.println("Изначальный массив: ");
+        printArray(arrayOfNums);
+
+        Iterator reverseArray = arrayOfNums.descendingIterator();
+        System.out.println("Перевертнутый массив: ");
+        printIterator(reverseArray);
+
+    }
+
+    public static void printArray(LinkedList<Integer> array) {
+        for (int i = 0; i < array.size(); i++) {
+            System.out.print(array.get(i) + " ");
+        }
+
+        System.out.println("");
+    }
+
+    public static void printIterator(Iterator array) {
+        while (array.hasNext()) {
+            System.out.print(array.next() + " ");
+        }
+
+        System.out.println("");
+    }
+
+    public static LinkedList<Integer> getRandomArray(int n) {
+        LinkedList<Integer> numbersArray = new LinkedList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < n; i++) {
+            numbersArray.add(random.nextInt(101) - 15);
+        }
+
+        return numbersArray;
+    }
+
+}
